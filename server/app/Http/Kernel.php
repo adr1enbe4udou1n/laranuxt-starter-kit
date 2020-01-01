85e554015206aa68fc153c513088226393fd2332
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-01-01 14:18:46 +0100

    up deps & cors

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
